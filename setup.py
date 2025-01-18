2370cacf125f4f2826ee895991e31aef3cbef6d5
sanoj c sam <sanojcsam123@gmail.com>
2025-01-18 15:03:00 +0530

    folder structure created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages ,setup
+
+
+setup(
+    name= "GENERATIVE AI PROJECT",
+    version='0.0.0',
+    auhtor = 'sanoj c sam',
+    author_email = "sanojcsam123@gmail.com",
+    packages= find_packages(),
+    install_requires=[]
+)
